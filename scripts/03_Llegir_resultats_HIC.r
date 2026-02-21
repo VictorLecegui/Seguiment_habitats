d4881739db8d13b0df1f5aab35d0999c5be410f8
VictorLecegui <viclecegui@gmail.com>
2026-02-21 17:36:43 +0100

    Abans de buscar punts per grups
    Comit abans de començar els scripts de generar punts per grups de CORINE

--- scripts/03_Llegir_resultats_HIC.r
+++ scripts/03_Llegir_resultats_HIC.r
@@ -11,7 +11,7 @@ path_punts <- "results/02_Loop_punts_HIC/Punts_mostreig"
 # List all RDS files (important fix)
 punts_hic <- list.files(path_punts, pattern = "\\.rds$", full.names = TRUE)
 
-file_path<-"results/02_Loop_punts_HIC/Punts_mostreig/Punts_X9340_MED.rds"
+#file_path<-"results/02_Loop_punts_HIC/Punts_mostreig/Punts_X9340_MED.rds"
 # Function to read and process each file
 read_points_grts <- function(file_path){
     print(file_path)
@@ -75,14 +75,45 @@ if("sp_design" %in% class(punts_obj)){
 # Apply to all files
 punts_ls <- lapply(punts_hic, read_points_grts)
 
-punts_ls[[1]]
+
 
 punts_mostreig <- bind_rows(punts_ls)
 nrow(punts_mostreig) # Nombre de punts a mostrejar
 
+colnames(punts_mostreig)
+
 # Nombre de punts aprofitats
 punts_mostreig  |> filter(Point_origin=="Legacy") |> nrow()
 
+st_write(punts_mostreig, "results/03_Llegir_resultats_HIC/punts_mostreig.gpkg")
 
-st_write(punts_mostreig, 
-          "results/03_Llegir_resultats_HIC/Punts_mostreig_HIC.shp")
+# st_write(punts_mostreig, 
+#           "results/03_Llegir_resultats_HIC/Punts_mostreig_HIC.shp")
+
+#### LLegir i resumir els punts de mosteig seleccionats. 
+
+
+punts_mostreig <- read_sf("results/03_Llegir_resultats_HIC/punts_mostreig.gpkg")
+colnames(punts_mostreig_esri)
+
+punts_mostreig |> 
+    group_by(COD_HIC_tf, RegioHIC) |> 
+    summarise(N = n()) |> 
+    arrange(desc(N))
+
+# Seguent nivell: Grup de CORINEs
+
+# Grups pels quals s'haurà de buscar punts
+punts_mostreig |> 
+    group_by(Codi_grup) |> 
+    summarise(N = n()) |>
+    filter(N < 30) |>  
+    arrange(desc(N))
+
+# Grups que ja tenen 30 punts
+punts_mostreig |> 
+    group_by(Codi_grup) |> 
+    summarise(N = n()) |>
+    filter(N >= 30) |>  
+    arrange(desc(N))
+length(unique(punts_mostreig$Codi_grup))
